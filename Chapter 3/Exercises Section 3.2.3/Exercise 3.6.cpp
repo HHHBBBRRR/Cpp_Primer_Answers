4a9625e64cdfbb87e14e618c81d4a3acdca4302a
HHHBBBRRR <90436322+HHHBBBRRR@users.noreply.github.com>
2022-03-13 20:10:06 +0800

    Update Exercise 3.6.cpp

--- Chapter 3/Exercises Section 3.2.3/Exercise 3.6.cpp
+++ Chapter 3/Exercises Section 3.2.3/Exercise 3.6.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
+#include<string>
+#include<cctype>
 
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::getline;
+
+int main()
+{
+	string s;
+
+	while (cin >> s)
+		;
+	for (auto &c : s)
+	{
+		c = 'X';
+	}
+
+	cout << s << endl;
+
+	return 0;
+}
